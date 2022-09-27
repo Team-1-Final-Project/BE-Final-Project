0af25af302abfd84a6ae3c4f99d1b3817e12024c
glooooow <2809yun@gmail.com>
2022-09-27 19:25:07 +0900

    220927 board entity, controller
    first commit that wrote entity controller

--- src/main/java/com/innovation/backend/controller/BoardController.java
+++ src/main/java/com/innovation/backend/controller/BoardController.java
@@ -1,4 +1,55 @@
 package com.innovation.backend.controller;
 
+import com.innovation.backend.dto.request.BoardRequestDto;
+import com.innovation.backend.dto.response.ResponseDto;
+import com.innovation.backend.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/board")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDto<?> getAllBoards() {
+        return boardService.getAllBoard();
+    }
+
+    @PostMapping("/board")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseDto<?> createBoard(@RequestPart BoardRequestDto requestDto, @RequestPart MultipartFile multipartFile, HttpServletRequest request) {
+        return boardService.createBoard(requestDto, multipartFile, request);
+    }
+
+    @GetMapping("/board/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDto<?> getBoard(@PathVariable Long id) {
+        return boardService.getBoard(id);
+    }
+
+    @PutMapping("/board/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseDto<?> alterBoard(@PathVariable Long id, @RequestPart BoardRequestDto requestDto, @RequestPart MultipartFile multipartFile, HttpServletRequest request) {
+        return boardService.alterBoard(id, requestDto, request);
+    }
+
+    @DeleteMapping("/board/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseDto<?> deleteBoard(@PathVariable Long id, HttpServletRequest request) {
+        return boardService.deleteBoard(id, request);
+    }
+
+
+
+
+
 }
--- src/main/java/com/innovation/backend/entity/Board.java
+++ src/main/java/com/innovation/backend/entity/Board.java
@@ -1,12 +1,13 @@
 package com.innovation.backend.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -17,5 +18,29 @@ public class Board extends Timestamped{
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @JoinColumn(name = "member_id", nullable = false)
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Member member;
+
+  @Column(nullable = false)
+  private String title;
+
+  @ElementCollection
+  @CollectionTable(name = "boardImage_id")
+//  @OneToMany(fetch = FetchType.LAZY)
+  private List<String> boardImage = new ArrayList<>();
+
+  @Column(nullable = false)
+  private String content;
+
+  @Column(nullable = false)
+  private int heartBoardNums;
+
+  @OneToMany(mappedBy = "board", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<HeartBoard> heartBoardList;
+
+  @OneToMany(mappedBy = "board", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<TagBoard> tagBoardList;
 
 }
