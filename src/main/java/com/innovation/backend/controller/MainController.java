3b2d5b40c91c783fcb4d830188d14e4d7fcf42c2
Ahnjaewon <67565707+Ahnjaewon@users.noreply.github.com>
2022-09-28 13:39:30 +0900

    Main update
    신규모임 조회 추가

--- src/main/java/com/innovation/backend/controller/MainController.java
+++ src/main/java/com/innovation/backend/controller/MainController.java
@@ -1,13 +1,15 @@
 package com.innovation.backend.controller;
 
 import com.innovation.backend.dto.response.DailyMissionResponseDto;
+import com.innovation.backend.dto.response.MeetingResponseDto;
 import com.innovation.backend.dto.response.ResponseDto;
 import com.innovation.backend.enums.ErrorCode;
 import com.innovation.backend.exception.CustomErrorException;
 import com.innovation.backend.jwt.UserDetailsImpl;
 import com.innovation.backend.repository.DailyMissionRepository;
 import com.innovation.backend.repository.MemberRepository;
-import com.innovation.backend.service.DailyMissionService;
+import com.innovation.backend.service.MainService;
+import com.innovation.backend.service.MeetingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,20 +17,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class MainController {
 
     private final DailyMissionRepository dailyMissionRepository;
-    private final DailyMissionService dailyMissionService;
+    private final MainService mainService;
     private final MemberRepository memberRepository;
+    private final MeetingService meetingService;
 
     // 데일리 미션 조회
     @GetMapping("/main/daily")
     public ResponseDto<DailyMissionResponseDto> dailyMission(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         DailyMissionResponseDto dailyMissionResponseDto;
-        dailyMissionResponseDto = dailyMissionService.dailyMission(userDetails);
+        dailyMissionResponseDto = mainService.dailyMission(userDetails);
         return ResponseDto.success(dailyMissionResponseDto);
     }
 
@@ -37,7 +42,7 @@ public class MainController {
     public ResponseDto<DailyMissionResponseDto> successMission(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         try {
             DailyMissionResponseDto dailyMissionResponseDto;
-            dailyMissionResponseDto = dailyMissionService.successMission(userDetails);
+            dailyMissionResponseDto = mainService.successMission(userDetails);
             return ResponseDto.success(dailyMissionResponseDto);
         } catch (
                 CustomErrorException e) {
@@ -48,5 +53,17 @@ public class MainController {
             return ResponseDto.fail(ErrorCode.NEED_LOGIN);
         }
 
+    }
+
+    // 신규 모임 조회
+    @GetMapping("/main/newmeeting")
+    public ResponseDto<List<MeetingResponseDto>> getNewMeeting() {
+        List<MeetingResponseDto> meetingResponseDtoList;
+        try {
+            meetingResponseDtoList = mainService.getNewMeeting();
+        } catch (Exception e) {
+            return ResponseDto.fail(ErrorCode.INVALID_ERROR);
+        }
+        return ResponseDto.success(meetingResponseDtoList);
     }
 }
--- src/main/java/com/innovation/backend/service/MainService.java
+++ src/main/java/com/innovation/backend/service/MainService.java
@@ -1,12 +1,15 @@
 package com.innovation.backend.service;
 
 import com.innovation.backend.dto.response.DailyMissionResponseDto;
+import com.innovation.backend.dto.response.MeetingResponseDto;
 import com.innovation.backend.entity.DailyMission;
+import com.innovation.backend.entity.Meeting;
 import com.innovation.backend.entity.Member;
 import com.innovation.backend.enums.ErrorCode;
 import com.innovation.backend.exception.CustomErrorException;
 import com.innovation.backend.jwt.UserDetailsImpl;
 import com.innovation.backend.repository.DailyMissionRepository;
+import com.innovation.backend.repository.MeetingRepository;
 import com.innovation.backend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,17 +17,21 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class DailyMissionService {
+public class MainService {
 
     private final DailyMissionRepository dailyMissionRepository;
     private final MemberRepository memberRepository;
+    private final MeetingRepository meetingRepository;
 
     LocalDateTime startDatetime = LocalDateTime.of(LocalDate.now(), LocalTime.of(0, 0, 0));
     LocalDateTime endDatetime = LocalDateTime.of(LocalDate.now(), LocalTime.of(23, 59, 59));
 
+    // 데일리 미션 조회
     public DailyMissionResponseDto dailyMission(UserDetailsImpl userDetails) {
         DailyMission dailyMission = dailyMissionRepository.findFirstByOrderByIdDesc();
         Boolean isDaily = false;
@@ -43,7 +50,7 @@ public class DailyMissionService {
         return new DailyMissionResponseDto(dailyMission, clearCount, isDaily);
     }
 
-
+    // 데일리 미션 성공
     public DailyMissionResponseDto successMission(UserDetailsImpl userDetails) {
 
         String userEmail = userDetails.getUsername();
@@ -62,5 +69,18 @@ public class DailyMissionService {
         }
 
         return new DailyMissionResponseDto(dailyMission, isDaily);
+    }
+
+    // 신규 모임 조회
+    public List<MeetingResponseDto> getNewMeeting() {
+        List<Meeting> meetingList = meetingRepository.findTop4ByOrderByCreatedAtDesc();
+        List<MeetingResponseDto> meetingResponseDtoList = new ArrayList<>();
+
+        for (Meeting meeting : meetingList) {
+            MeetingResponseDto meetingResponseDto = new MeetingResponseDto(meeting);
+            meetingResponseDtoList.add(meetingResponseDto);
+        }
+
+        return meetingResponseDtoList;
     }
 }
