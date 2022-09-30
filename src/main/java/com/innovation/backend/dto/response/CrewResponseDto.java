223314c36557a856f4880d99214a2432a1546a4b
decaf-latte <hyejin.jeon940120@gmail.com>
2022-09-30 10:29:52 +0900

    [ADD] crew Response Dto add profile Image

--- src/main/java/com/innovation/backend/dto/response/CrewResponseDto.java
+++ src/main/java/com/innovation/backend/dto/response/CrewResponseDto.java
@@ -6,19 +6,21 @@ import lombok.Getter;
 @Getter
 
 public class CrewResponseDto {
-  private final Long crewId;
+
 
   private final Long memberId;
   private final Long meetingId;
   private final String email;
   private final String nickname;
+  private final String profileImage;
 
   public CrewResponseDto(Crew crew){
-    this.crewId = crew.getId();
+
     this.memberId = crew.getMember().getId();
     this.meetingId = crew.getMeeting().getId();
     this.email = crew.getMember().getEmail();
     this.nickname = crew.getMember().getNickname();
+    this.profileImage = crew.getMember().getProfileImage();
   }
 
 }
