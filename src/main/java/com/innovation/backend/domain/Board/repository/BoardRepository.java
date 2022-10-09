0d0f4fc3eb1af8b43d7310bcfd796275ec312e8c
glooooow <2809yun@gmail.com>
2022-10-09 19:40:25 +0900

    221009 getAllBoard infinteScroll & creatBoard correct
    infiniteScroll & createBoard image == null > return null

--- src/main/java/com/innovation/backend/domain/Board/repository/BoardRepository.java
+++ src/main/java/com/innovation/backend/domain/Board/repository/BoardRepository.java
@@ -2,8 +2,14 @@ package com.innovation.backend.domain.Board.repository;
 
 import com.innovation.backend.domain.Board.domain.Board;
 import com.innovation.backend.domain.Member.domain.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -11,13 +17,26 @@ import java.util.Optional;
 public interface BoardRepository extends JpaRepository<Board,Long> {
 
     Optional<Board> findById(Long id);
+
     Board findBoardById(Long id);
+
     Board deleteBoardById(Long id);
+
     Optional<Board> findAllByMember(Member member);
+
     List<Board> findAllByOrderByHeartBoardNumsDesc();
 
-//    List<Board> findAllByTagBoard();
+    //    List<Board> findAllByTagBoard();
     List<Board> findAllByOrderByCreatedAtDesc();
+
     List<Board> findTop4ByCreatedAtBetweenOrderByHeartBoardNumsDesc(LocalDateTime start, LocalDateTime end);
+
     List<Board> findByMember(Member member);
+
+    Slice<Board> findByIdLessThanAndOrderByIdDesc(Long id);
+    Page<Board> findByIdLessThanAndIdEqualsOrderByDesc(Long id);
+    long countBoardById(Long id);
+
+    Slice<Board> findByIdLessThanAndOrderByIdDesc(Long id, Pageable pageable);
 }
+
