a2e818776b87f7f8a28e27b2123ed19b5628755c
decaf-latte <hyejin.jeon940120@gmail.com>
2022-10-17 01:25:33 +0900

    CI/CD test controller #2

--- src/main/java/com/innovation/backend/domain/Search/SearchController.java
+++ src/main/java/com/innovation/backend/domain/Search/SearchController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SearchController {
 
-  @GetMapping
+  @GetMapping("/testtest")
   public ResponseDto<String> test (){
     return ResponseDto.success("성공입니다.");
   }
