a9d182a10e73b358fed04918dc251222c18d02fb
decaf-latte <hyejin.jeon940120@gmail.com>
2022-10-17 13:24:02 +0900

    CI/CD 수정 #3

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
-#JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
-JAR_FILE="java"
+PROJECT_ROOT=/home/ubuntu/app
+cd $REPOSITORY
+
+JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+#JAR_FILE="java"
+
+APP_NAME=spring-webapp.jar
 
 TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 # 프로세스가 켜져 있으면 종료
 kill -9 $CURRENT_PID
