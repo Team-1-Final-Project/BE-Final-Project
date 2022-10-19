582d439fb3f79275d62f4429f691d4bb62412c23
decaf-latte <hyejin.jeon940120@gmail.com>
2022-10-19 21:12:10 +0900

    Search Meeting & Board By Title Complete

--- src/main/java/com/innovation/backend/domain/Search/SearchController.java
+++ src/main/java/com/innovation/backend/domain/Search/SearchController.java
@@ -1,9 +1,15 @@
 package com.innovation.backend.domain.Search;
 
+import com.innovation.backend.domain.Board.dto.response.BoardResponseDto;
+import com.innovation.backend.domain.Board.service.BoardService;
+import com.innovation.backend.domain.Meeting.dto.response.MeetingGetAllResponseDto;
+import com.innovation.backend.domain.Meeting.dto.response.MeetingResponseDto;
 import com.innovation.backend.global.common.response.ResponseDto;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -11,4 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SearchController {
 
+  private final SearchService searchService;
+
+  //게시글 검색하기
+  @GetMapping("/board/search")
+  public ResponseDto<List<BoardResponseDto>> searchBoard (@RequestParam(value = "keyword") String keyword) {
+    return ResponseDto.success(searchService.searchBoard(keyword));
+  }
+
+  //모임검색하기
+  @GetMapping("/meeting/search")
+  public ResponseDto<List<MeetingGetAllResponseDto>> searchMeeting (@RequestParam(value = "keyword") String keyword) {
+    return ResponseDto.success(searchService.searchMeeting(keyword));
+  }
 }
--- src/main/java/com/innovation/backend/domain/Search/SearchService.java
+++ src/main/java/com/innovation/backend/domain/Search/SearchService.java
@@ -1,6 +1,15 @@
 package com.innovation.backend.domain.Search;
 
+import com.innovation.backend.domain.Board.domain.Board;
+import com.innovation.backend.domain.Board.dto.response.BoardResponseDto;
+import com.innovation.backend.domain.Board.repository.BoardRepository;
+import com.innovation.backend.domain.Meeting.domain.Meeting;
+import com.innovation.backend.domain.Meeting.dto.response.MeetingGetAllResponseDto;
+import com.innovation.backend.domain.Meeting.repository.MeetingRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +17,30 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @AllArgsConstructor
 public class SearchService {
+  private final BoardRepository boardRepository;
+  private final MeetingRepository meetingRepository;
 
   //게시글 검색
+  public List<BoardResponseDto> searchBoard (String keyword){
+
+    List<Board> boardList = boardRepository.findByTitleContainsIgnoreCase(keyword);
+
+    List<BoardResponseDto> boardResponseDtoList = new ArrayList<>();
+    for(Board board : boardList){
+      boardResponseDtoList.add(new BoardResponseDto(board));
+    }
+    return boardResponseDtoList;
+  }
 
   //모임 검색
+  public List<MeetingGetAllResponseDto> searchMeeting (String keyword){
 
+    List<Meeting> meetingList = meetingRepository.findByTitleContainsIgnoreCase(keyword);
+
+    List<MeetingGetAllResponseDto> meetingGetAllResponseDtos = new ArrayList<>();
+    for (Meeting meeting : meetingList){
+      meetingGetAllResponseDtos.add(new MeetingGetAllResponseDto(meeting));
+    }
+    return meetingGetAllResponseDtos;
+  }
 }
