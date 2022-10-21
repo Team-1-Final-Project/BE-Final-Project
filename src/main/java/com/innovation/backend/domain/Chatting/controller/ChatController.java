1cd78df45c991227f001ab8dec87711b80bd4208
decaf-latte <hyejin.jeon940120@gmail.com>
2022-10-21 15:58:30 +0900

    chat service & controller

--- src/main/java/com/innovation/backend/domain/Chatting/controller/ChatController.java
+++ src/main/java/com/innovation/backend/domain/Chatting/controller/ChatController.java
@@ -1,31 +1,17 @@
 package com.innovation.backend.domain.Chatting.controller;
 
-import com.innovation.backend.domain.Chatting.dto.ChatDto;
-import com.innovation.backend.domain.Chatting.service.ChatService;
-import com.innovation.backend.global.common.response.ResponseDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.http.HttpStatus;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-public class ChatController {
-
-//    private final SimpMessageSendingOperations messageForm;
-    private final ChatService chatService;
-
-    @GetMapping("/meeting/{meetingId}/chatroom")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseDto<Slice<ChatDto>> chatLoad(@PathVariable("meetingId") Long meetingId, Pageable pageable) {
-        return chatService.chatLoad(meetingId, pageable);
-    }
+//@RestController
+//@RequiredArgsConstructor
+//public class ChatController {
+//
+////    private final SimpMessageSendingOperations messageForm;
+//    private final ChatService chatService;
+//
+//    @GetMapping("/meeting/{meetingId}/chatroom")
+//    @ResponseStatus(HttpStatus.OK)
+//    public ResponseDto<Slice<ChatDto>> chatLoad(@PathVariable("meetingId") Long meetingId, Pageable pageable) {
+//        return chatService.chatLoad(meetingId, pageable);
+//    }
 
 //    @MessageMapping("/chat/message")
 //    public void meetingChat(ChatDto chatDto) {
@@ -34,4 +20,4 @@ public class ChatController {
 //        messageForm.convertAndSend(
 //                "/sub/chat/" + chatDto);
 //    }
-}
+//}
--- src/main/java/com/innovation/backend/domain/Chatting/service/ChatService.java
+++ src/main/java/com/innovation/backend/domain/Chatting/service/ChatService.java
@@ -7,53 +7,51 @@ import com.innovation.backend.domain.Meeting.domain.Meeting;
 import com.innovation.backend.domain.Meeting.repository.MeetingRepository;
 import com.innovation.backend.global.common.response.ResponseDto;
 import com.innovation.backend.global.enums.ErrorCode;
-import lombok.RequiredArgsConstructor;
-import org.openqa.selenium.Alert;
-import org.springframework.data.domain.*;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.simp.annotation.SendToUser;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.security.auth.message.MessageInfo;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-public class ChatService {
-
-    private final Chat chat;
-    private final ChatRepository chatRepository;
-    private final MeetingRepository meetingRepository;
-
-    public ResponseDto<Slice<ChatDto>> chatLoad(Long meetingId, Pageable pageable) {
-
-        Meeting meeting = isPresentMeeting(meetingId);
-
-        if(null == meeting) {
-            return ResponseDto.fail(ErrorCode.NOT_FOUND_MEETING);
-        }
-
-        Slice<Chat> chatting = chatRepository.findAllByOrderByIdDesc(pageable); {
-            PageRequest.of(0, 20, Sort.by(Sort.Direction.DESC, "id"));
-        }
-        List<ChatDto> chatDtoList = new ArrayList<>(chatRepository.findAllByOrderByIdDesc());
-
-        return ResponseDto.success(new PageImpl<>(chatDtoList, pageable, chatting.getNumberOfElements()));
-    }
-
-    @Transactional(readOnly = true)
-    public Meeting isPresentMeeting(Long meetingId) {
-        Optional<Meeting> optionalMeeting = meetingRepository.findById(meetingId);
-        return optionalMeeting.orElse(null);
-    }
-
-//    @Transactional
-//    public void saveChat(ChatDto chatDto) {
+//@Service
+//@RequiredArgsConstructor
+//public class ChatService {
 //
-//        chatRepository.save(new Chat(chatDto));
+//    private final Chat chat;
+//    private final ChatRepository chatRepository;
+//    private final MeetingRepository meetingRepository;
+//
+//    public ResponseDto<Slice<ChatDto>> chatLoad(Long meetingId, Pageable pageable) {
+//
+//        Meeting meeting = isPresentMeeting(meetingId);
+//
+//        if(null == meeting) {
+//            return ResponseDto.fail(ErrorCode.NOT_FOUND_MEETING);
+//        }
+//
+//        Slice<Chat> chatting = chatRepository.findAllByOrderByIdDesc(pageable); {
+//            PageRequest.of(0, 20, Sort.by(Sort.Direction.DESC, "id"));
+//        }
+//        List<ChatDto> chatDtoList = new ArrayList<>(chatRepository.findAllByOrderByIdDesc());
+//
+//        return ResponseDto.success(new PageImpl<>(chatDtoList, pageable, chatting.getNumberOfElements()));
 //    }
-}
+//
+//    @Transactional(readOnly = true)
+//    public Meeting isPresentMeeting(Long meetingId) {
+//        Optional<Meeting> optionalMeeting = meetingRepository.findById(meetingId);
+//        return optionalMeeting.orElse(null);
+//    }
+//
+////    @Transactional
+////    public void saveChat(ChatDto chatDto) {
+////
+////        chatRepository.save(new Chat(chatDto));
+////    }
+//}
